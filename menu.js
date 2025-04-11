2e09d54f41e5d5232790910712a1393f3630e363
Copper <158495678+Copper90@users.noreply.github.com>
2025-04-10 16:52:24 -0800

    Update menu.js

---
+++ menu.js
@@ -0,0 +1,17 @@
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>uMenu</title>
+    </head>
+    <body>
+    <div id="container"></div>
+    <script>
+        const iframe = document.createElement('iframe');
+        iframe.src = 'https://j4ham3z.github.io/uMenu/';
+        iframe.width = '800';
+        iframe.height = '600';
+        iframe.style.border = '1px solid black';
+        document.getElementById('container').appendChild(iframe);
+    </script>
+    </body>
+    </html>
