c5a0ca9e7a488a110d57f48311f5ed4e82f3d1ed
Copper <158495678+Copper90@users.noreply.github.com>
2025-04-10 17:04:05 -0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 document.addEventListener("keydown", function (e) {
     const iframe = document.createElement('iframe');
-    iframe.src = 'https://j4ham3z.github.io/uMenu/';
+    iframe.src = 'https://j4ham3z.github.io/uMenu/index.html';
     iframe.width = '800';
     iframe.height = '600';
     iframe.style.border = '1px solid black';
